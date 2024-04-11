d54905caa51de8f70d29766dcbfe60416aa289a8
mustafaghaffar763@gmail.com <mustafaghaffar763@gmail.com>
2024-04-11 07:55:59 +0500

    Adding 45 assignments

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,169 @@
-#! /usr/bin/env node
+//  <==== #Assignments 1 Install Node.js, TypeScript and VS Code ====>
+
+// console.log(`Already Completed!`);
+
+//  <=======================>
+
+//  <==== #Assignments 2 Personal Message: ====>
+
+// let personName:string="Zane";
+// console.log(`Hello ${personName}, would you like to have some cake?`);
+
+//  <=======================>
+
+//   <==== #Assignments 3 Name Cases: ====>
+
+// let personName:string="zAnE blAck";
+// console.log(`LowerCase: `,personName.toLowerCase());
+// console.log(`UpperCase: `,personName.toUpperCase());
+// console.log("TitleCase: ",personName.toLowerCase().replace(/\b\w/g,c=>c.toUpperCase()));
+
+//  <=======================>
+
+//   <==== #Assignments 4 Famous Quote: ====>
+// let author:string=`George Bernard Shaw`;
+// let quote:string=`"Cricket is a game played by 11 fools and watched by 11,000 fools"`
+// console.log(`${author} one said, ${quote}`);
+//  <=======================>
+
+//   <==== #Assignments 5 Famous Quote 2 ====>
+
+// let famousPerson:string=`George Bernard Shaw`;
+// let message:string=`${famousPerson} once said,"Cricket is a game played by 11 fools and watched by 11,000 fools"`
+// console.log(message)
+
+//  <=======================>
+
+//  <==== #Assignments 6 Stripping Names:====>
+// let personName:string="\n\t    Zane Black\t\n";
+// console.log(`"Original": `,personName)
+// console.log(`"Stripped": `,personName.trim())
+//  <=======================>
+//  <==== #Assignments 7 Number Eight: ====>
+// console.log(5 + 3);
+// console.log(11 - 3);
+// console.log(8*1);
+// console.log(24/3);
+//  <=======================>
+//  <==== #Assignments 8 ====>
+// Done in Assignment 7!
+//  <=======================>
+//  <==== #Assignments 9 Favorite Number:====>
+// Author: Mustafa
+// Date: 4/11/2024
+/* Description: Storing my favorite number in a variable. Then, using that variable and creating a message that reveals my favorite number.*/
+// let favouriteNumber:number=27;
+// console.log(`${favouriteNumber} is my favourite number.`)
+//  <=======================>
+//  <==== #Assignments 10 Adding Comments: ====>
+//                  /\
+// In assignment 9  | an 11 |
+//                         \/
+//  <=======================>
+//  <==== #Assignments 11 ====>
+// Author: Mustafa
+// Date: 4/11/2024
+/* Description: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.*/
+// Method 1:
+// let friendNames:string[]=['Zane','Micheal','Dave','John']
+// console.log(friendNames[0]);
+// console.log(friendNames[1]);
+// console.log(friendNames[2]);
+// console.log(friendNames[3]);
+// Method 2:
+// for(let i=0;i<friendNames.length;i++){
+//     console.log(friendNames[i])
+// }
+//  <=======================>
+//  <==== #Assignments 12 Greetings: ====>
+// Method 1:
+// let friendNames:string[]=['Zane','Micheal','Dave','John']
+// for(let i=0;i<friendNames.length;i++){
+//     console.log(`Hello ${friendNames[i]}, wanna hangout at my place tonight?`)
+// }
+// Method 2:
+// let message:string=`wanna hangout at my place tonight?`;
+// console.log(friendNames[0]+" "+message);
+// console.log(friendNames[1]+" "+message);
+// console.log(friendNames[2]+" "+message);
+// console.log(friendNames[3]+" "+message);
+//  <=======================>
+//  <==== #Assignments 13 Your Own Array: ====>
+// let transportation:string[]=["Honda Motorcycle","Audi","Lexus"]
+// transportation.forEach((elements)=>{console.log(`I would like to own a a ${elements}`)});
+//  <=======================>
+//  <==== #Assignments 14 Guest List:====>
+// let GuestList:string[]=['Zane','Micheal','Dave','John']
+// GuestList.forEach((element)=>{
+//     console.log(`Hello ${element}, wanna have dinner at my place tonight?`);
+// })
+//  <=======================>
+//  <==== #Assignments 15 Changing Guest List:====>
+let GuestList:string[]=['Zane','Micheal','Dave','John']
+GuestList.forEach((element)=>{
+    console.log(`Hello ${element}, wanna have dinner at my place tonight?`);
+});
+console.log(Math.floor(Math.random()*(GuestList.length-1)))
+// let randomguest:number=Math.floor(Math.random()*(GuestList.length-1))
+// console.log(`Oh no looks like ${GuestList[randomguest]}, wont be able to have  dinner at my place.`);
+//  <=======================>
+//  <====  #Assignments 16 ====>
+//  <=======================>
+//  <====  #Assignments 17 ====>
+//  <=======================>
+//  <==== #Assignments 18 ====>
+//  <=======================>
+//  <====  #Assignments 19 ====>
+//  <=======================>
+//  <====  #Assignments 20 ====>
+//  <=======================>
+//  <====  #Assignments 21 ====>
+//  <=======================>
+//  <==== #Assignments 22 ====>
+//  <=======================>
+//  <====  #Assignments 23 ====>
+//  <=======================>
+//  <====  #Assignments 24 ====>
+//  <=======================>
+//  <====  #Assignments 25 ====>
+//  <=======================>
+//  <====  #Assignments 26 ====>
+//  <=======================>
+//  <====  #Assignments 27 ====>
+//  <=======================>
+//  <====  #Assignments 28 ====>
+//  <=======================>
+//  <====  #Assignments 29 ====>
+//  <=======================>
+//  <====  #Assignments 30 ====>
+//  <=======================>
+//  <==== #Assignments 31 ====>
+//  <=======================>
+//  <====  #Assignments 32 ====>
+//  <=======================>
+//  <====  #Assignments 33 ====>
+//  <=======================>
+//  <====  #Assignments 34 ====>
+//  <=======================>
+//  <====  #Assignments 35 ====>
+//  <=======================>
+//  <====  #Assignments 36 ====>
+//  <=======================>
+//  <==== #Assignments 37 ====>
+//  <=======================>
+//  <====  #Assignments 38 ====>
+//  <=======================>
+//  <====  #Assignments 39 ====>
+//  <=======================>
+//  <====  #Assignments 40 ====>
+//  <=======================>
+//  <====  #Assignments 41  ====>
+//  <=======================>
+//  <==== #Assignments 42  ====>
+//  <=======================>
+//  <====  #Assignments 43 ====>
+//  <=======================>
+//  <====  #Assignments 44 ====>
+//  <=======================>
+//  <====  #Assignments 45 ====>
+//  <=======================>
